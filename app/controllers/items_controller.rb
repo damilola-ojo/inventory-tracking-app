2974ff765815b60a3587caccfc85fde0e577c35b
Damilola Ojo <tdami262@gmail.com>
2022-06-26 03:23:25 +0100

    Generate Items Controller

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,26 @@
+class ItemsController < ApplicationController
+  
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+end
