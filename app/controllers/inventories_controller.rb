a95dd3e1a54342f96b0c00a1add3561bf6225bd1
Damilola Ojo <tdami262@gmail.com>
2022-06-26 07:02:28 +0100

    Implement CRUD actions for Inventories

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,26 +1,53 @@
-class InventoryController < ApplicationController
+class InventoriesController < ApplicationController
   
   def index
-
+    @inventory = Inventory.all
   end
 
   def new
-
+    @inventory = Inventory.new
   end
 
   def edit
-
+    @inventory = Inventory.find(params[:id])
   end
 
   def create
+    @inventory = Inventory.create(inventory_params)
 
+    if @inventory.save
+      redirect_to inventories_path
+    else 
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def update
+    @inventory = Inventory.find(params[:id])
 
+    unless params[:inventory][:base_location_id] == @inventory.base_location_id 
+      new_base_location = @inventory.locations.find(params[:inventory][:base_location_id])
+
+      @inventory.change_base_location_to(new_base_location)
+    end
+
+    if @inventory.update(inventory_params)
+      redirect_to inventories_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @inventory = Inventory.find(params[:id])
+    
+    @inventor.destroy
 
+    redirect_to inventories_path, status: :see_other
   end
+
+  private
+    def inventory_params
+      params.required(:inventory).permit(:id, :name)
+    end
 end
