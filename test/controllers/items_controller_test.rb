c09a426017332395f9ca6dde63815639def1d1d3
Damilola Ojo <tdami262@gmail.com>
2022-06-26 19:20:11 +0100

    Add Items Controller Test

--- test/controllers/items_controller_test.rb
+++ test/controllers/items_controller_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class ItemsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @inventory = Inventory.first
+    @item = @inventory.items.first
+  end
+
+  test "should get new" do
+    get new_inventory_item_path(@inventory)
+    assert_response :success
+  end
+
+  test "should get show" do
+    get inventory_item_path(@inventory, @item)
+    assert_response :success
+  end
+
+  test "should get edit" do 
+    get edit_inventory_item_path(@inventory, @item)
+    assert_response :success
+  end
 end
