a368620e47d2643c7c5eb76a8d5079d544ac67e9
Damilola Ojo <tdami262@gmail.com>
2022-06-26 02:09:54 +0100

    Add Base Location to Inventory

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,5 +1,7 @@
 class Inventory < ApplicationRecord
   has_many :locations, dependent: :destroy
+  has_one :base_location, -> { where(base: true) }, 
+    class: "Location"
   has_many :items, dependent: :destroy
   validates :name, presence: true
 end
