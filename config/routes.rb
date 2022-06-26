fb098161b4f198b09a66c19f2c434d83cb862db3
Damilola Ojo <tdami262@gmail.com>
2022-06-26 03:42:26 +0100

    Update Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :inventories, except: :show do
+    resources :items, except: :index
+    resources :locations
+  end
 end
