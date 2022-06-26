c3e47eafafc1ef3791ce14092553cd7463e579dd
Damilola Ojo <tdami262@gmail.com>
2022-06-26 08:15:46 +0100

    Implement CRUD actions for Items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,26 +1,52 @@
 class ItemsController < ApplicationController
   
-  def index
-
-  end
-
   def show
-
+    @item = Item.find(params[:id])
+    @inventory = Inventory.find(params[:inventory_id])
   end
 
   def new
-
+    @inventory = Inventory.find(params[:inventory_id])
+    @item = @inventory.items.build
   end
 
   def edit
-
+    @inventory = Inventory.find(params[:inventory_id])
+    @item = Item.find(params[:id])
   end
 
   def create
+    @inventory = Inventory.find(params[:inventory_id])
+    @item = @inventory.items.build(item_params)
 
+    if @item.save
+      redirect_to inventory_items_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def update
-
+    @inventory = Inventory.find(params[:inventory_id])
+    @item = Item.find(params[:id])
+    
+    if @item.update(item_params)
+      redirect_to inventory_item_path(@inventory, @item)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
+
+  def destroy
+    @item = Item.find(params[:id])
+
+    @item.destroy 
+
+    redirect_to inventory_items_path
+  end
+
+  private
+    def item_params 
+      params.required(:item).permit(:id, :name, :quantity)
+    end
 end
